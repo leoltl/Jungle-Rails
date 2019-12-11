210d12d7ef67fe3f0e5d12ac5f6b8f75040fd14a
Leo Lee <tl.leolee@gmail.com>
2019-12-11 17:29:33 +0000

    added user login logout functions, added login status on nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     # If the user exists AND the password entered is correct.
-    if user and user.authenticate(params[:password]):
+    if @user and @user.authenticate(params[:password])
       #Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      redirect_to '/'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/'
   end
 end
