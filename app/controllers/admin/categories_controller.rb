1aa6e4d235ae8baace1d5394abede8e36b77c6d6
Leo Lee <tl.leolee@gmail.com>
2019-12-11 16:06:25 +0000

    added admin Category page, update top nav

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,5 +4,25 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      flash[:notice] = 'Category created!'
+      redirect_to [:admin, :categories]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
